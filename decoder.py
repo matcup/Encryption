d617a572e7cce10465678a264fcdb486bc0a4dbe
sharkfork <506793708@qq.com>
2019-11-04 16:39:46 +0800

    Add key
    添加十位数字秘钥

--- decoder.py
+++ decoder.py
@@ -1,24 +1,32 @@
 import numpy as np
 
+key = list('15234567890')
+
 
 def decoder(text):
-    decoder = []
+    decoder_num = []
+    decoder_str = []
     for i in range(len(text)):
-        decode_i = chr(int(text[i])-10)
-        decoder.append(decode_i)
-    decoder = ''.join(decoder)
+        k = key[i % 10]
+        decode_i = ord(text[i]) - int(k)
+        decoder_num.append(decode_i)
+
+    for j in range(len(decoder_num)):
+        a = chr(decoder_num[j])
+        decoder_str.append(a)
+        decoder = ''.join(decoder_str)
     return decoder
-	
+
 
 def str_trans_list(text):
-	a = []
-	b = []
-	a = text.strip('[').strip(']').split(', ')
-	for i in range(len(a)):
-		b.append(int(a[i]))
-	return b 
-	
-	
+    text_list = []
+
+    for i in range(len(text)):
+        b = text[i]
+        text_list.append(b)
+    return text_list
+
+
 decode_char = input('输入字符：')
 decode_char = str_trans_list(decode_char)
 outputs = decoder(decode_char)
--- encoder.py
+++ encoder.py
@@ -1,15 +1,23 @@
 import numpy as np
 
+key = list('15234567890')
+outputs = []
+
 
 def encoder(text):
     encode = []
     for i in range(len(text)):
         encode_i = ord(text[i])
-        encode.append(encode_i+10)
+        k = key[i % 10]
+        encode.append(encode_i + int(k))
     np.array(encode, dtype=np.uint8)
     return encode
-	
+
 
 encode_char = input('输入字符：')
-outputs = encoder(encode_char)
-print(outputs)
+output = encoder(encode_char)
+for i in range(len(encode_char)):
+    c = chr(output[i])
+    outputs.append(c)
+
+print(''.join(outputs))
